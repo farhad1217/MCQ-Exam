a059d840423921ed92fef2d23d629a77f4b36b38
farhad1217 <146172553+farhad1217@users.noreply.github.com>
2023-09-27 14:07:25 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const ans = ["A", "B", "C", "D", "D", "C", "B", "A", "B", "D"];
+let total = ans.length;
+
+function yesNO(qName){
+    for(var i = 0; i < 4; i++){
+        var x = qName + i;
+        if(document.getElementById("x").checked)
+            return document.getElementById("x").value;
+    }
+    return "E";
+}
+
+function score(){
+    let scoree = 0;
+    for(var i = 0; i<total; i++){
+        let qName = document.getElementById("q" + i+1);
+        if(yesNO(qName) === ans[i])
+            scoree ++;
+    }
+    return scoree;
+}
+
+function checkResult(){
+    document.getElementById("hh").innerHTML = "Your Score is " + score() + " out of " + total;
+}
